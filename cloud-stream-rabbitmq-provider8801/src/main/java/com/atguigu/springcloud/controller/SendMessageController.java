e054559a7cfb885ba077755b710dcd5e492a05a0
Qi4o <1303759265@qq.com>
2022-09-09 17:38:22 +0800

    new file

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/controller/SendMessageController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author Qiao
+ * @Create 2022/9/7 16:22
+ */
+
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage(){
+        return messageProvider.send();
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/IMessageProvider.java
@@ -0,0 +1,9 @@
+package com.atguigu.springcloud.service;
+
+/**
+ * @Author Qiao
+ * @Create 2022/9/7 16:18
+ */
+public interface IMessageProvider {
+    public String send();
+}
