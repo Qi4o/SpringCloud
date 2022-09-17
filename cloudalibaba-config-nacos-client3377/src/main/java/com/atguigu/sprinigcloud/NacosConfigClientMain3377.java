e3e5577fd552ed0d5acd8a04200933d9d275df88
Qi4o <1303759265@qq.com>
2022-09-17 17:12:01 +0800

    new file

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/sprinigcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.atguigu.sprinigcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Qiao
+ * @Create 2022/9/16 16:25
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
