f1168d3be354e16be4bbd0c1aaf4587aecc903db
Qi4o <1303759265@qq.com>
2022-09-20 10:52:29 +0800

    new file

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Qiao
+ * @Create 2022/9/18 09:04
+ */
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Qiao
+ * @Create 2022/9/18 09:06
+ */
+
+@RestController
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA(){
+        return "-----testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        return "-----testB";
+    }
+}
